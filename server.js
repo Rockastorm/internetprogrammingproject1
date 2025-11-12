c07428d640be3b1c0ee0017705474d4c7bcd161b
Rockastorm <Jacobb.teamxstudios@gmail.com>
2025-11-11 20:09:03 -0500

    fixed users and views

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express =
 require('express');
 
+const userRouter =
+require('./routes/users');
+
 const app = express(); //calling this function sets up a server
+
+app.set('view engine', 'ejs');
+app.use('/users', userRouter);
 
 app.get('/', (req,res)=>{
     console.log('Here');
-    res.render("index", {text:"World"});
+    res.render("index", {user:"Jacob"});
 }); //this function will run when someone goes to the root folder
 
 app.listen(3030);
