c689ca6dac0bf5bdf7bf52822277dd7c1bcca4a2
Rockastorm <Jacobb.teamxstudios@gmail.com>
2025-11-11 20:46:07 -0500

    Added post list and post creation links and pages

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
+
 const express =
 require('express');
 
 const userRouter =
 require('./routes/users');
 
+const postsRouter =
+require('./routes/posts');
+
 const app = express(); //calling this function sets up a server
 
 app.set('view engine', 'ejs');
 app.use('/users', userRouter);
+app.use('/posts', postsRouter);
 
 app.get('/', (req,res)=>{
     console.log('Here');
-    res.render("index", {user:"Jacob"});
+    res.render("index", {user:"User: Jacob"});
 }); //this function will run when someone goes to the root folder
+
 
 app.listen(3030);
