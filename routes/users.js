9c8455b7c623aa25602b4ed9b5785322310e794e
Rockastorm <Jacobb.teamxstudios@gmail.com>
2025-11-12 14:07:54 -0500

    updated user id

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,19 @@ router.get('/',(req,res)=>{
 router.get('/new',(req,res)=>{
     res.send('New User Form');
 });
-router.post('/:id', (req, res)=>{
+// router.get('/:id', (req, res)=>{
+//    res.send(`Getting User data: ${req.params.id}`);
+// });
+router.post("/:id").get((req, res)=>{
     res.send(`Getting User data: ${req.params.id}`)
+}).delete((req, res)=>{
+    res.send(`Deleting user with id: ${req.params.id}`)
+}).put((req, res)=>{
+    res.send(`Updating user with id: ${req.params.id}`)
+});
+
+router.param("id", (req, res, next, id)=>{
+    console.log(`Acessing user #${id}`);
+    next();
 });
 module.exports = router;
